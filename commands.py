b66388d0c07c03b45f30afe30b0a4932f5c5604e
ry10hu <115399801+ry10hu@users.noreply.github.com>
2024-04-05 17:58:15 +0000

    Improved command system.

--- commands.py
+++ commands.py
@@ -5,22 +5,24 @@ def userInput():
     while True:
         drugtable.money = int(math.floor(float(drugtable.money)))
         user_input = input(colours.questioncolour + "\nWhat would you like to do? " + colours.inputcolour).lower()
+        user_input = user_input.split()
+        print(user_input)
         # print('\n')
-        if user_input == 'change stock':
+        if user_input[0] == 'changestock':
             for x in range(0, len(drugtable.table)):
                 number = random.randint(-20, 20)
                 if drugtable.table[x][1] + number > 0:
                     drugtable.table[x][1] += number    
         drugtable.money=int(drugtable.money)
-        if user_input == "buy" or user_input == 'b':
-            buying = input(colours.questioncolour + '\nWhat would you like to buy? (nvm to cancel) ' + colours.inputcolour)
-            if buying == 'nvm':
-                continue
 
-            amount = input(colours.questioncolour + '\nHow much would you like to buy? (nvm to cancel) ' + colours.inputcolour)
+        if user_input[0] == "buy" or user_input[0] == 'b':
+            if len(user_input) == 3:
+                buying = user_input[1]
+                amount = user_input[2]
+            else:
+                print(colours.answercolour + f'\nERROR: WRONG SYNTAX. The Correct Syntax Is: buy drug amount' + colours.inputcolour)
+                continue
             try:
-                if amount == 'nvm':
-                    continue
                 if amount == 'max':
                     amount = int(drugtable.money/drugtable.table[nametoid.toID(buying)][1])
                 else:
@@ -35,15 +37,14 @@ def userInput():
                 print('\nEnter a numeric value or a shorthand (max). \n')
                 continue
 
-        if user_input == 'sell' or user_input == 's':
-            selling = input(colours.questioncolour + '\nWhat would you like to sell? (nvm to cancel) ' + colours.inputcolour)
-            if selling == 'nvm':
-              continue
-            
-            amount = input(colours.questioncolour + '\nHow much would you like to sell? (nvm to cancel) ' + colours.inputcolour)
+        if user_input[0] == 'sell' or user_input[0] == 's':
+            if len(user_input) == 3:
+                selling = user_input[1]
+                amount = user_input[2]
+            else:
+                print(colours.answercolour + f'\nERROR: WRONG SYNTAX. The Correct Syntax Is: sell drug amount' + colours.inputcolour)
+
             try:
-                if amount == 'nvm':
-                    continue
                 if amount == 'max':
                     amount = drugtable.table[nametoid.toID(selling)][2]
                 else:
@@ -57,18 +58,18 @@ def userInput():
                 print('\nEnter a numeric value or a shorthand (max). \n')
                 continue
 
-        if user_input == 'inventory' or user_input == 'i':
+        if user_input[0] == 'inventory' or user_input[0] == 'i':
             
             for x in range(0, len(drugtable.table)):
                 print(colours.answercolour + f'\n{drugtable.table[x][3]} Owned:  {drugtable.table[x][2]}      Current Price: {drugtable.table[x][1]}' + colours.inputcolour)
             
 
-        if user_input == 'balance' or user_input == 'bal':
+        if user_input[0] == 'balance' or user_input[0] == 'bal':
             print(colours.answercolour + f'\nYou have {drugtable.money} money.' + Style.RESET_ALL)
-        if user_input == 'price':
-            price = input(colours.questioncolour+ '\nPrice of what? ' + colours.inputcolour)
+        if user_input[0] == 'price':
+            price = user_input[1]
             print(colours.answercolour + f'\nThe price of {price} is {drugtable.table[nametoid.toID(price)][1]} money.' + Style.RESET_ALL)
-        if user_input == 'cls' or user_input == 'clear':
+        if user_input[0] == 'cls' or user_input[0] == 'clear':
             if platform.system() == "Windows":
                os.system('cls')
             elif platform.system() in ["Linux", "Darwin"]:  
