425091e488948bf9396119eccb807167a2cfff43
ry10hu <115399801+ry10hu@users.noreply.github.com>
2024-04-13 16:43:41 +0000

    Updating Code

--- commands.py
+++ commands.py
@@ -6,70 +6,75 @@ def userInput():
         drugtable.money = int(math.floor(float(drugtable.money)))
         user_input = input(colours.questioncolour + "\nWhat would you like to do? " + colours.inputcolour).lower()
         # print('\n')
-        if user_input == 'change stock':
-            for x in range(0, len(drugtable.table)):
-                number = random.randint(-20, 20)
-                if drugtable.table[x][1] + number > 0:
-                    drugtable.table[x][1] += number    
-        drugtable.money=int(drugtable.money)
-        if user_input == "buy" or user_input == 'b':
-            buying = input(colours.questioncolour + '\nWhat would you like to buy? (nvm to cancel) ' + colours.inputcolour)
-            if buying == 'nvm':
-                continue
+        if len(user_input) >= 1:            
+            if user_input[0] == 'save':
+                print(colours.answercolour + '\nSaving...' + colours.inputcolour)
+                with open('saves/save.xml', 'w') as f:
+                    f.write(drugtable.Bs_save.prettify())
+                print(colours.answercolour + '\nSaved!' + colours.inputcolour)
+            if user_input[0] == 'changestock':
+                for x in range(0, len(drugtable.savetable)):
+                    number = random.randint(-20, 20)
+                    if drugtable.savetable[x][1] + number > 0:
+                        drugtable.savetable[x][1] += number    
+            drugtable.money=int(drugtable.money)
 
-            amount = input(colours.questioncolour + '\nHow much would you like to buy? (nvm to cancel) ' + colours.inputcolour)
-            try:
-                if amount == 'nvm':
+            if user_input[0] == "buy" or user_input[0] == 'b':
+                if len(user_input) == 3:
+                    buying = user_input[1]
+                    amount = user_input[2]
+                else:
+                    print(colours.answercolour + '\nERROR: WRONG SYNTAX. The Correct Syntax Is: buy {drug} {amount}' + colours.warningcolour)
                     continue
-                if amount == 'max':
-                    amount = int(drugtable.money/drugtable.table[nametoid.toID(buying)][1])
-                else:
-                    amount = int(amount)
-                if drugtable.money >= amount * drugtable.table[nametoid.toID(buying)][1]:
-                    drugtable.money -= amount * drugtable.table[nametoid.toID(buying)][1]
-                    drugtable.table[nametoid.toID(buying)][2] += amount
-                else:
-                    req = amount * drugtable.table[nametoid.toID(buying)][1] - drugtable.money
-                    print(colours.answercolour + f'\nGrind harder, you broke idiot. (You need {req} more money)' + colours.inputcolour)
-            except ValueError:
-                print('\nEnter a numeric value or a shorthand (max). \n')
-                continue
-
-        if user_input == 'sell' or user_input == 's':
-            selling = input(colours.questioncolour + '\nWhat would you like to sell? (nvm to cancel) ' + colours.inputcolour)
-            if selling == 'nvm':
-              continue
-            
-            amount = input(colours.questioncolour + '\nHow much would you like to sell? (nvm to cancel) ' + colours.inputcolour)
-            try:
-                if amount == 'nvm':
+                try:
+                    if amount == 'max':
+                        amount = int(drugtable.money/drugtable.savetable[nametoid.toID(buying)][1])
+                    else:
+                        amount = int(amount)
+                    if drugtable.money >= amount * drugtable.savetable[nametoid.toID(buying)][1]:
+                        drugtable.money -= amount * drugtable.savetable[nametoid.toID(buying)][1]
+                        drugtable.savetable[nametoid.toID(buying)][2] += amount
+                    else:
+                        req = amount * drugtable.savetable[nametoid.toID(buying)][1] - drugtable.money
+                        print(colours.answercolour + f'\nGrind harder, you broke idiot. (You need {req} more money)' + colours.inputcolour)
+                except ValueError:
+                    print('\nEnter a numeric value or a shorthand (max). \n')
                     continue
-                if amount == 'max':
-                    amount = drugtable.table[nametoid.toID(selling)][2]
-                else:
-                    amount = int(amount)
-                if drugtable.table[nametoid.toID(selling)][2] >= amount:
-                    drugtable.money += amount * drugtable.table[nametoid.toID(selling)][1]
-                    drugtable.table[nametoid.toID(selling)][2]-= amount
-                else:
-                    print(colours.answercolour + '\nYou can\'t sell what you don\'t have, idiot.' + colours.inputcolour)
-            except ValueError:
-                print('\nEnter a numeric value or a shorthand (max). \n')
-                continue
 
-        if user_input == 'inventory' or user_input == 'i':
-            
-            for x in range(0, len(drugtable.table)):
-                print(colours.answercolour + f'\n{drugtable.table[x][3]} Owned:  {drugtable.table[x][2]}      Current Price: {drugtable.table[x][1]}' + colours.inputcolour)
-            
+            if user_input[0] == 'sell' or user_input[0] == 's':
+                if len(user_input) == 3:
+                    selling = user_input[1]
+                    amount = user_input[2]
+                else:
+                    print(colours.answercolour + '\nERROR: WRONG SYNTAX. The Correct Syntax Is: sell {drug} {amount}' + colours.inputcolour)
 
-        if user_input == 'balance' or user_input == 'bal':
-            print(colours.answercolour + f'\nYou have {drugtable.money} money.' + Style.RESET_ALL)
-        if user_input == 'price':
-            price = input(colours.questioncolour+ '\nPrice of what? ' + colours.inputcolour)
-            print(colours.answercolour + f'\nThe price of {price} is {drugtable.table[nametoid.toID(price)][1]} money.' + Style.RESET_ALL)
-        if user_input == 'cls' or user_input == 'clear':
-            if platform.system() == "Windows":
-               os.system('cls')
-            elif platform.system() in ["Linux", "Darwin"]:  
-                os.system('clear')
+                try:
+                    if amount == 'max':
+                        amount = drugtable.savetable[nametoid.toID(selling)][2]
+                    else:
+                        amount = int(amount)
+                    if drugtable.savetable[nametoid.toID(selling)][2] >= amount:
+                        drugtable.money += amount * drugtable.savetable[nametoid.toID(selling)][1]
+                        drugtable.savetable[nametoid.toID(selling)][2]-= amount
+                    else:
+                        print(colours.answercolour + '\nYou can\'t sell what you don\'t have.' + colours.inputcolour)
+                except ValueError:
+                    print('\nEnter a numeric value or a shorthand (max). \n')
+                    continue
+
+            if user_input[0] == 'inventory' or user_input[0] == 'i':
+                
+                for x in range(0, len(drugtable.savetable)):
+                    print(colours.answercolour + f'\n{drugtable.savetable[x][3]} Owned:  {drugtable.savetable[x][2]}      Current Price: {drugtable.savetable[x][1]}' + colours.inputcolour)
+                
+
+            if user_input[0] == 'balance' or user_input[0] == 'bal':
+                print(colours.answercolour + f'\nYou have {drugtable.money} money.' + Style.RESET_ALL)
+            if user_input[0] == 'price':
+                price = user_input[1]
+                print(colours.answercolour + f'\nThe price of {price} is {drugtable.savetable[nametoid.toID(price)][1]} money.' + Style.RESET_ALL)
+            if user_input[0] == 'cls' or user_input[0] == 'clear':
+                if platform.system() == "Windows":
+                    os.system('cls')
+                elif platform.system() in ["Linux", "Darwin"]:  
+                    os.system('clear')
--- drugtable.py
+++ drugtable.py
@@ -1,10 +1,29 @@
 global table # ID, Price, Amount, Name
 global money
+from bs4 import BeautifulSoup
+with open('saves/save.xml', 'r') as f:
+    save = f.read()
+
+Bs_save = BeautifulSoup(save, "xml")
+
+# Find the meth tag
+meth = Bs_save.find('meth')
+cocaine = Bs_save.find('cocaine')
+heroin = Bs_save.find('heroin')
+marijuana = Bs_save.find('marijuana')
+opium = Bs_save.find('opium')
 money = 100
-table = [
+savetable = [
     [0, 10, 0, 'Meth'],
     [1, 20, 0, 'Cocaine'],
     [2, 30, 0, 'Heroin'],
     [3, 40, 0, 'Marijuana'],
     [4, 50, 0, 'Opium'],
+]
+loadtable = [
+    [0, meth['price'], 0, 'Meth'],
+    [1, cocaine['price'], 0, 'Cocaine'],
+    [2, heroin['price'], 0, 'Heroin'],
+    [3, marijuana['price'], 0, 'Marijuana'],
+    [4, opium['price'], 0, 'Opium'],
 ]
